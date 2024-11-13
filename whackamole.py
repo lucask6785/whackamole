cdb32b09baf2c4c7a6bbc000858fa6d63f91f33a
Lucas Kilday <lucask6785@gmail.com>
2024-11-13 17:14:56 -0500

    created whackamole game

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,16 @@
 import pygame
+import random
 
+def draw_grid(screen):
+    for i in range(20):
+        pygame.draw.line(screen, (0,0,0), ((i+1)*32, 0), ((i+1)*32, 512))
+    for i in range(16):
+        pygame.draw.line(screen, (0,0,0), (0, (i+1)*32), (640, (i+1)*32))
+
+def initialize_board():
+    board =  [["-" for i in range(20)] for i in range(16)]
+    board[0][0] = 'm'
+    return board
 
 def main():
     try:
@@ -9,12 +20,27 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        current_position = [0, 0]
+        board = initialize_board()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+                    col = x // 32
+                    row = y // 32
+                    if board[row][col] == 'm':
+                        board[row][col] = '-'
+                        new_row = random.randrange(0, 16)
+                        new_col = random.randrange(0, 20)
+                        current_position = [new_row, new_col]
+                        board[new_row][new_col] = 'm'
+
             screen.fill("light green")
+            draw_grid(screen)
+            screen.blit(mole_image, mole_image.get_rect(topleft=(current_position[1]*32,current_position[0]*32)))
             pygame.display.flip()
             clock.tick(60)
     finally:
